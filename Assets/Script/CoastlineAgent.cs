3bd159f69027b760784b574bc631ea02d20d2f02
FreddyGitHM <alfredorusso71@gmail.com>
2022-06-01 21:42:31 +0200

    Code refactoring

--- Assets/Script/CoastlineAgent.cs
+++ Assets/Script/CoastlineAgent.cs
@@ -6,39 +6,37 @@ using System.Linq;
 [RequireComponent(typeof(Terrain))]
 public class CoastlineAgent : MonoBehaviour
 {
-    struct Agent
+    private readonly struct Agent
     {
-        int vertex;
+        private readonly int _vertex;
 
         public Agent(int vertex)
         {
-            this.vertex = vertex;
+            _vertex = vertex;
         }
 
-        public int getVertex()
+        public int GetVertex()
         {
-            return vertex;
+            return _vertex;
         }
     }
 
     //Terrain data
-    private Terrain terrain;
-    private TerrainData td;
-    private int x;
-    private int y;
-    private float[,] heightmap;
+    private Terrain _terrain;
+    private TerrainData _td;
+    private int _x;
+    private int _y;
+    private float[,] _heightmap;
 
     //Coastline agent
     public bool startingFromMapCenter;
-    public int CoastlineTokens;
-    public int VertexLimit;
+    public int coastlineTokens;
+    public int vertexLimit;
     [Range(0.03f, 1.0f)] public float maxHeight;
-    private Queue agents;
-    private Vector2Int _center;
-    private bool _firstTime;
+    private Queue _agents;
 
     // Directions vector
-    Vector2Int[] directions = new Vector2Int[]
+    private readonly Vector2Int[] _directions =
     {
         Vector2Int.up,
         Vector2Int.down,
@@ -51,116 +49,105 @@ public class CoastlineAgent : MonoBehaviour
     };
 
     // variable for testing
-    private int elevatedVertex = 0;
+    private int _elevatedVertex;
 
     void Start()
     {
         //Getting terrain information
-        terrain = GetComponent<Terrain>();
-        td = terrain.terrainData;
-        x = td.heightmapResolution;
-        y = td.heightmapResolution;
+        _terrain = GetComponent<Terrain>();
+        _td = _terrain.terrainData;
+        _x = _td.heightmapResolution;
+        _y = _td.heightmapResolution;
 
-        //Initialize heighmap
-        heightmap = new float[x, y];
+        //Initialize heightmap
+        _heightmap = new float[_x, _y];
 
         // Initialize Queue
-        agents = new Queue();
+        _agents = new Queue();
 
         // Instantiate first agent
-        Agent agent = new Agent((x - 1) * (y - 1));
+        Agent agent = new Agent((_x - 1) * (_y - 1));
         GetCoastlineAgents(agent);
+        Debug.Log("Number of agents: " + _agents.Count);
 
-        Debug.Log("Number of agents: " + agents.Count);
-
-        if (CoastlineTokens * agents.Count > x * y)
+        if (coastlineTokens * _agents.Count > _x * _y)
         {
             Debug.Log("Number of Tokens too high, reduce it");
             return;
         }
-
-        _firstTime = true;
 
         StartCoroutine(Action());
     }
 
     private void GetCoastlineAgents(Agent agent)
     {
-        if (agent.getVertex() >= VertexLimit)
+        if (agent.GetVertex() >= vertexLimit)
         {
             for (int i = 0; i < 2; i++)
             {
-                GetCoastlineAgents(new Agent(agent.getVertex() / 2));
+                GetCoastlineAgents(new Agent(agent.GetVertex() / 2));
             }
         }
         else
         {
-            agents.Enqueue(agent);
+            _agents.Enqueue(agent);
         }
     }
 
     private IEnumerator Action()
     {
-        while (agents.Count != 0)
+        while (_agents.Count != 0)
         {
-            Vector2Int location = getCoastlinePoints();
+            _ = (Agent)_agents.Dequeue();
+            
+            Vector2Int location = GetCoastlinePoints();
 
-            // Store the center of the island (starting point)
-            if (_firstTime)
-            {
-                _center = location;
-                _firstTime = false;
-                Debug.Log("Map Center: " + location);
-            }
-
-            Agent agent = (Agent)agents.Dequeue();
-
-            for (int i = 0; i < CoastlineTokens; i++)
+            for (int i = 0; i < coastlineTokens; i++)
             {
                 // Points to score
-                List<Vector2Int> candidates = nearPoints(location);
+                List<Vector2Int> candidates = NearPoints(location);
 
                 while (candidates.Count == 0)
                 {
-                    location = getCoastlinePoints();
-                    candidates = nearPoints(location);
+                    location = GetCoastlinePoints();
+                    candidates = NearPoints(location);
                 }
 
                 List<float> scores = new List<float>();
 
-                for (int j = 0; j < candidates.Count; j++)
+                foreach (Vector2Int candidate in candidates)
                 {
-                    Vector2 attractor = getAttractor();
-                    Vector2 repulsor = getRepulsor(attractor);
-                    scores.Add(scorePoint(candidates[j], agent, attractor, repulsor));
+                    Vector2 attractor = GetAttractor();
+                    Vector2 repulsor = GetRepulsor(attractor);
+                    scores.Add(ScorePoint(candidate, attractor, repulsor));
                 }
 
                 location = candidates[scores.IndexOf(scores.Max())];
-                heightmap[location.y, location.x] = Random.Range(.02f, maxHeight);
-                elevatedVertex++;
+                _heightmap[location.y, location.x] = Random.Range(.02f, maxHeight);
+                _elevatedVertex++;
             }
 
-            td.SetHeights(0, 0, heightmap);
+            _td.SetHeights(0, 0, _heightmap);
             yield return new WaitForEndOfFrame();
         }
 
         yield return new WaitForEndOfFrame();
 
-        Debug.Log("Vertex Elevated: " + elevatedVertex);
+        Debug.Log("Vertex Elevated: " + _elevatedVertex);
 
         yield return SmoothingAgent.Instance.Action();
     }
 
-    private Vector2Int getCoastlinePoints()
+    private Vector2Int GetCoastlinePoints()
     {
         List<Vector2Int> coastlinePoints = new List<Vector2Int>();
 
-        for (int i = 0; i < x; i++)
+        for (int i = 0; i < _x; i++)
         {
-            for (int j = 0; j < y; j++)
+            for (int j = 0; j < _y; j++)
             {
                 Vector2Int tmp = new Vector2Int(i, j);
-                if (heightmap[tmp.y, tmp.x] != 0 && checkNearPoint(tmp))
+                if (_heightmap[tmp.y, tmp.x] != 0 && CheckNearPoint(tmp))
                 {
                     coastlinePoints.Add(tmp);
                 }
@@ -171,23 +158,22 @@ public class CoastlineAgent : MonoBehaviour
         {
             return coastlinePoints[Random.Range(0, coastlinePoints.Count)];
         }
-        
-        // If here means that the first agent is placed on the terrain
 
+        // If here means that the first agent is placed on the terrain
         if (!startingFromMapCenter)
         {
-            return new Vector2Int(Random.Range(0, x), Random.Range(0, y));
+            return new Vector2Int(Random.Range(0, _x), Random.Range(0, _y));
         }
-        
-        return new Vector2Int(x / 2, y / 2);
+
+        return new Vector2Int(_x / 2, _y / 2);
     }
 
-    private bool checkNearPoint(Vector2Int location)
+    private bool CheckNearPoint(Vector2Int location)
     {
-        foreach (Vector2Int point in directions)
+        foreach (Vector2Int point in _directions)
         {
             Vector2Int tmp = point + location;
-            if (heightmap[tmp.y, tmp.x] == 0)
+            if (_heightmap[tmp.y, tmp.x] == 0)
             {
                 return true;
             }
@@ -196,14 +182,14 @@ public class CoastlineAgent : MonoBehaviour
         return false;
     }
 
-    private List<Vector2Int> nearPoints(Vector2Int location)
+    private List<Vector2Int> NearPoints(Vector2Int location)
     {
         List<Vector2Int> nearestPoints = new List<Vector2Int>();
 
-        foreach (Vector2Int point in directions)
+        foreach (Vector2Int point in _directions)
         {
             Vector2Int tmp = location + point;
-            if (checkLimit(tmp) && heightmap[tmp.y, tmp.x] == 0)
+            if (CheckLimit(tmp) && _heightmap[tmp.y, tmp.x] == 0)
             {
                 nearestPoints.Add(tmp);
             }
@@ -212,31 +198,31 @@ public class CoastlineAgent : MonoBehaviour
         return nearestPoints;
     }
 
-    private Vector2 getAttractor()
+    private Vector2 GetAttractor()
     {
-        return new Vector2(Random.Range(0, x), Random.Range(0, y));
+        return new Vector2(Random.Range(0, _x), Random.Range(0, _y));
     }
 
-    private Vector2 getRepulsor(Vector2 attractor)
+    private Vector2 GetRepulsor(Vector2 attractor)
     {
-        return new Vector2((x - 1) - attractor.x, (y - 1) - attractor.y);
+        return new Vector2((_x - 1) - attractor.x, (_y - 1) - attractor.y);
     }
 
-    private float scorePoint(Vector2 point, Agent agent, Vector2 attractor, Vector2 repulsor)
+    private float ScorePoint(Vector2 point, Vector2 attractor, Vector2 repulsor)
     {
         float result = Mathf.Pow(Vector2.Distance(point, repulsor), 2.0f) -
                        Mathf.Pow(Vector2.Distance(point, attractor), 2.0f) +
-                       (3 * Mathf.Pow(getClosestDistance(point, agent), 2.0f));
+                       (3 * Mathf.Pow(GetClosestDistance(point), 2.0f));
         return result;
     }
 
-    private float getClosestDistance(Vector2 point, Agent agent)
+    private float GetClosestDistance(Vector2 point)
     {
         //Order of the point inside the array to respect the Point point received from input: left, right, up, down 
-        Vector2[] borderPoints = new Vector2[]
+        Vector2[] borderPoints =
         {
-            new Vector2(0, point.y), new Vector2(x, point.y),
-            new Vector2(point.x, y), new Vector2(point.x, 0)
+            new Vector2(0, point.y), new Vector2(_x, point.y),
+            new Vector2(point.x, _y), new Vector2(point.x, 0)
         };
 
         float[] result = new float[borderPoints.Length];
@@ -248,9 +234,9 @@ public class CoastlineAgent : MonoBehaviour
         return result.OrderBy(a => a).ToArray()[0];
     }
 
-    private bool checkLimit(Vector2Int point)
+    private bool CheckLimit(Vector2Int point)
     {
-        if (point.x >= 0 && point.x <= x - 1 && point.y >= 0 && point.y <= y - 1)
+        if (point.x >= 0 && point.x <= _x - 1 && point.y >= 0 && point.y <= _y - 1)
         {
             return true;
         }
